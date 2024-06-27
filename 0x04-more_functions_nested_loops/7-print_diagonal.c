aac53aef469c4fac892fad4bee47081e1c270330
ouidane <130859522+ouidane@users.noreply.github.com>
2024-06-27 23:56:09 +0100

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,30 @@
-#include"main.h"
+#include "holberton.h"
 
 /**
- * print_diagonal - print a diagonal line
- *
- * @n: is the number of times the \ character
- *     should be printed
-*/
-
+ * print_diagonal - function to print symbols
+ * @n : integer value
+ * Return: Always 0.
+ */
 void print_diagonal(int n)
 {
-	int postn, space;
+	int a, b;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	if (n > 0)
 	{
-		for (postn = 1; postn < n; ++postn)
+		for (a = 0; a <= n; a++)
 		{
-			for (space = 1; space < postn; ++space)
+			for (b = 1; b <= a; b++)
+			{
+				if (b != a)
 				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
+				else if (b == a)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+			}
 		}
 	}
+	else
+	_putchar('\n');
 }
