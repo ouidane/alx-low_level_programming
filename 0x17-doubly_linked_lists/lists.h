0b248b2d0e519638cbeba499d053512d26f81183
ouidane <ouidanezakaria@gmail.com>
2024-09-19 21:35:06 +0100

    task 1

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS
+#define LISTS
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,6 +21,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
