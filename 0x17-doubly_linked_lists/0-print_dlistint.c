d57670adbeee72fde446a7a42d4446d19873e7db
ouidane <ouidanezakaria@gmail.com>
2024-09-19 21:50:22 +0100

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,16 +8,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count;
+	size_t count = 0;
 
-	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-
-    return (count);
+	return (count);
 }
