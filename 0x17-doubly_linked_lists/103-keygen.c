96569d994a2b13de08103284ddbc45cbfae894a9
ouidane <ouidanezakaria@gmail.com>
2024-09-20 01:34:55 +0100

    update

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,43 +3,55 @@
 #include <string.h>
 
 /**
- * main - Entry point
- * @argc: argument count
- * @argv: argument vector
+ * main - Generates a key for crackme5 based on the given username
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success), 1 if incorrect usage
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i, b;
-	size_t len, add;
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char p[7] = "      ";
-
+	size_t username_len, char_sum;
+	unsigned int i, product, random_value;
+	char *lookup_table = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char key[7] = "      ";
+	
 	if (argc != 2)
 	{
 		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-	len = strlen(argv[1]);
-	p[0] = l[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)
-		add += argv[1][i];
-	p[1] = l[(add ^ 79) & 63];
-	for (i = 0, b = 1; i < len; i++)
-		b *= argv[1][i];
-	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len; i++)
-		if ((char)b <= argv[1][i])
-			b = argv[1][i];
-	srand(b ^ 14);
-	p[3] = l[rand() & 63];
-	for (b = 0, i = 0; i < len; i++)
-		b += argv[1][i] * argv[1][i];
-	p[4] = l[(b ^ 239) & 63];
-	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
-		b = rand();
-	p[5] = l[(b ^ 229) & 63];
-	printf("%s\n", p);
+
+	username_len = strlen(argv[1]);
+
+	key[0] = lookup_table[(username_len ^ 59) & 63];
+
+	for (i = 0, char_sum = 0; i < username_len; i++)
+		char_sum += argv[1][i];
+	key[1] = lookup_table[(char_sum ^ 79) & 63];
+
+	for (i = 0, product = 1; i < username_len; i++)
+		product *= argv[1][i];
+	key[2] = lookup_table[(product ^ 85) & 63];
+
+	char largest_char = argv[1][0];
+	for (i = 1; i < username_len; i++)
+	{
+		if (argv[1][i] > largest_char)
+			largest_char = argv[1][i];
+	}
+	srand(largest_char ^ 14);
+	key[3] = lookup_table[rand() & 63];
+
+	for (i = 0, product = 0; i < username_len; i++)
+		product += argv[1][i] * argv[1][i];
+	key[4] = lookup_table[(product ^ 239) & 63];
+
+	for (i = 0, random_value = 0; i < (unsigned int)argv[1][0]; i++)
+		random_value = rand();
+	key[5] = lookup_table[(random_value ^ 229) & 63];
+
+	printf("%s\n", key);
+
 	return (0);
 }
