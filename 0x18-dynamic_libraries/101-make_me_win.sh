f4ba6e055b20d8d12f035af0f3352cc94d67abc4
ouidane <ouidanezakaria@gmail.com>
2024-09-23 23:25:14 +0000

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ouidane/alx-low_level_programming/blob/main/0x18-dynamic_libraries/win.so
+wget -P /tmp https://github.com/ouidane/alx-low_level_programming/raw/refs/heads/main/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
