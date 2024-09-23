2e81802585a169df7d4921e0b7bff60b3d61f299
ouidane <ouidanezakaria@gmail.com>
2024-09-24 00:29:32 +0100

    task 3

--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int winning_numbers[] = {9, 8, 10, 24, 75, 9};
-int current_index = 0;
 
 /**
  * rand - Custom rand function to return winning numbers
  * 
  * Return: The next winning number
  */
-int rand(void)
-{
-    int result = winning_numbers[current_index];
-    current_index = (current_index + 1) % 6;
+int rand(void) {
+    static int numbers[] = {9, 8, 10, 24, 75, 9};
+    static int index = 0;
+    
+    int result = numbers[index];
+    index = (index + 1) % 6;
     return result;
-}
-
-/**
- * srand - Custom srand function (does nothing)
- * @seed: Seed value
- */
-void srand(unsigned int seed)
-{
-    (void)seed;
 }
