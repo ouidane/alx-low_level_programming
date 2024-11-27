1718b4f87dc90ba04ba61c6ef03b42076de7654e
ouidane <ouidanezakaria@gmail.com>
2024-11-27 22:50:23 +0100

    update task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""
-Module to calculate the perimeter of an island in a grid.
-"""
-
+"""Module to calculate the perimeter of an island in a grid."""
 
 def island_perimeter(grid):
     """
     Calculate the perimeter of an island in a grid.
 
     Args:
-        grid (list of list of int): A 2D grid where 1 represents
-        land and 0 represents water.
-
+        grid (list): A 2D grid represents an island.
     Returns:
         int: The perimeter of the island.
     """
@@ -21,20 +16,13 @@ def island_perimeter(grid):
 
     for r in range(rows):
         for c in range(cols):
-            # Check if current cell is land
             if grid[r][c] == 1:
-                # Check all 4 adjacent sides
-                # Top side
                 if r == 0 or grid[r-1][c] == 0:
                     perimeter += 1
-                # Bottom side
                 if r == rows-1 or grid[r+1][c] == 0:
                     perimeter += 1
-                # Left side
                 if c == 0 or grid[r][c-1] == 0:
                     perimeter += 1
-                # Right side
                 if c == cols-1 or grid[r][c+1] == 0:
                     perimeter += 1
-
     return perimeter
